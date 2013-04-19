75d16df4d4ffb3666b940bf990e724ad6b404919
Matthieu <matthieu@rimba-net.com>
2013-04-19 01:32:36 -0500

    Correction of the memory space initialization

--- init_memoryspace.m
+++ init_memoryspace.m
@@ -7,7 +7,7 @@ elseif nargin < 3
 end
 
 memory=zeros(K,N); %Creates an array of dimension K*N and fills it with zeros
-r = randi(1000-n,1,K); %Generates K random integers, one for each variant
+r = randi(N-n,1,K); %Generates K random integers, one for each variant
 for i=1:K; %For each variant
     for j=1:n; %For each continguous memory location
         memory(i,r(i)+j)=1;%We put ones wherever the vulnerable memory is
